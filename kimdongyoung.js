1cb1381a5bf9901fc50581f74f577f4e2ee41ba2
[서버]김동영 <dykim@birdletter.com>
2022-10-25 18:06:15 +0900

    step14

--- kimdongyoung.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector('button');
-let i = 1;
-
-button.addEventListener('click', event => {
-    i++;
-    document.querySelector(".img img-fluid").setAttribute("src", "profile/profile"+ i +".png")
-});
